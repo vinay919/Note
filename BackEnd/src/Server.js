7e43448378e01778f696ff2d77acd669ca4d118b
vinay.vishwakarma <vv3108@gmail.com>
2025-09-09 12:21:44 +0530

    fixed structure

--- BackEnd/src/Server.js
+++ BackEnd/src/Server.js
@@ -3,6 +3,23 @@ import express from 'express';
 const app = express();
 const PORT = 5001;
 
+//middleware (to parse json body)
+app.use(express.json())
+
+//Routes
+app.get("/note", (req, res) => {
+    res.status(200).json({ message: "Hello, Express.js!" });
+});
+app.post("/note", (req, res) => {
+    res.status(201).json({ message: "Data Received" })
+});
+app.put("/note/:id", (req, res) => {
+    res.status(200).json({ message: "Data Received" })
+});
+app.delete("/note/:id", (req, res) => {
+    res.status(200).json({ message: "Data Received" })
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 
