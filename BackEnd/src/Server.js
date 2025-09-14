4478b4ad3d9c688b482796bb0cb9eded0090556f
vinay919 <vv3108@gmail.com>
2025-09-14 13:37:27 +0530

    create frontend

--- BackEnd/src/Server.js
+++ BackEnd/src/Server.js
@@ -2,25 +2,28 @@ import express from 'express';
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
+import cors from 'cors'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5002
 
-connectDB();
 
 //middleware (to parse json body)
 app.use(express.json())
+app.use(cors())
 
 //Routes
 app.use("/api/notes", notesRoutes)
 
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-
+// first connect to DB and then start the server
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on http://localhost:${PORT}`);
+    })
 })
+
 
 
 
